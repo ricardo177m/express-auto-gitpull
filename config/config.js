9025c5b98f0771d27bbc6936cc03b0e99d6c36b7
unknown <rmoreiradev@gmail.com>
2022-01-16 14:34:41 +0000

    unregister test

--- config/config.js
+++ config/config.js
@@ -3,10 +3,6 @@ const CONFIG = {
   updateCommand: "cd <PATH> && git reset --hard HEAD && git pull",
   repos: [
     {
-      repoFullName: "rmoreiradev/test-auto-pull",
-      path: "/var/www/web.rmeev.tech/html/test-auto-pull"
-    },
-    {
       repoFullName: "rmoreiradev/express-auto-gitpull",
       path: "/var/www/pull.rmeev.tech/api"
     },
